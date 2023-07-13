51902741c065583e7dd2c07dcb6f230488e7a40f
Jonathan Mak <j.m4k119@gmail.com>
2023-07-13 13:14:16 -0400

    card structure created. will continue style after

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -3,8 +3,36 @@ import './Testimonials.scss';
 
 export default function Testimonials() {
     return (
-        <section className="testmonials">
-            
+        <section className="testimonials">
+            <div className="testimonials__card">
+                <div className="testimonials__quote-container">
+                    <p>The best thing about using Little Links is the amount of time we saved!</p>
+                </div>
+                <div className="testimonials__author-container">
+                    <p>Allison Wilcox</p>
+                    <p>Little Hands Montessori</p>
+                </div>
+            </div>
+
+            <div className="testimonials__card">
+                <div className="testimonials__quote-container">
+                    <p>The best thing about using Little Links is the amount of time we saved!</p>
+                </div>
+                <div className="testimonials__author-container">
+                    <p>Allison Wilcox</p>
+                    <p>Little Hands Montessori</p>
+                </div>
+            </div>
+
+            <div className="testimonials__card">
+                <div className="testimonials__quote-container">
+                    <p>The best thing about using Little Links is the amount of time we saved!</p>
+                </div>
+                <div className="testimonials__author-container">
+                    <p>Allison Wilcox</p>
+                    <p>Little Hands Montessori</p>
+                </div>
+            </div>
         </section>
         )
 
