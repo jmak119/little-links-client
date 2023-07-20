cac259a751e4496e9f9b20396a0697f8692f2370
Jonathan Mak <j.m4k119@gmail.com>
2023-07-20 15:56:06 -0400

    add profile pictures to overlay properly

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import './Testimonials.scss';
+import userAllison from "../../assets/images/testimonial-photo-allison.jpg";
+import userJoanna from "../../assets/images/testimonial-photo-joanna.webp"
+import userDavid from "../../assets/images/testimonial-photo-david.jpg"
 
 export default function Testimonials() {
     return (
@@ -8,6 +11,7 @@ export default function Testimonials() {
                 <div className="testimonials__quote-container">
                     <p>The best thing about using Little Links is the amount of time we saved!</p>
                 </div>
+                <img className="testimonials__avatar" src={userAllison}></img>
                 <div className="testimonials__author-container">
                     <p>Allison Wilcox</p>
                     <p>Little Hands Montessori</p>
@@ -18,9 +22,10 @@ export default function Testimonials() {
                 <div className="testimonials__quote-container testimonials__">
                     <p>The best thing about using Little Links is the amount of time we saved!</p>
                 </div>
+                <img className="testimonials__avatar" src={userJoanna}></img>
                 <div className="testimonials__author-container testimonials__yellow">
-                    <p>Allison Wilcox</p>
-                    <p>Little Hands Montessori</p>
+                    <p>Joanna Birch</p>
+                    <p>Starlight Kids Academy</p>
                 </div>
             </div>
 
@@ -28,9 +33,10 @@ export default function Testimonials() {
                 <div className="testimonials__quote-container">
                     <p>The best thing about using Little Links is the amount of time we saved!</p>
                 </div>
+                <img className="testimonials__avatar" src={userDavid}></img>
                 <div className="testimonials__author-container testimonials__orange">
-                    <p>Allison Wilcox</p>
-                    <p>Little Hands Montessori</p>
+                    <p>David Jones</p>
+                    <p>Little Sprouts Daycare</p>
                 </div>
             </div>
         </section>
